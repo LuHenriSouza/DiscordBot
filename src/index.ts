e284e8fc933d058c50bffce3791c5cdca12ac4c7
luizhapsouza <luizhapsouza@gmail.com>
2024-04-10 14:36:04 -0300

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { ExtendedClient } from './structs/ExtendedClient';
+export * from 'colors';
+import config from "./config.json";
+
+const client = new ExtendedClient();
+
+client.start();
+
+export { client }
+
+client.on('ready', () => {
+    console.log('Bot online'.green)
+})
+
+
+
+client.on('messageUpdate', (message) => {
+    if (!message.author) return;
+    if (message.author.id == client.user?.id) return;
+
+    message.reply({
+        content: "EDITA MENSAGEM NA MINHA FRENTE NÃO ARROMBADO DE MERDA"
+    })
+
+})
